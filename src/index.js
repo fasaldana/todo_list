2b4c9e81d693c062e70dde0afd25476ec94a22f2
fasaldana <fernando_s_98@hotmail.com>
2022-03-24 09:36:53 -0500

    Setup project with webpack

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import './style.css';
+
+const tasks = [
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+];
+
+function component() {
+  for (let i = 0; i < tasks.length; i++) {
+    const element = document.createElement('div');
+    element.classList.add('list-content');
+
+    const contentDiv = document.createElement('div');
+    contentDiv.classList.add('list-check');
+
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.id = 'check';
+
+    const desc = document.createElement('label');
+    desc.htmlFor = 'check';
+
+    const moveIcon = document.createElement('i');
+    moveIcon.classList.add('fa-solid');
+    moveIcon.classList.add('fa-ellipsis-vertical');
+
+    // Lodash, currently included via a script, is required for this line to work
+    desc.innerHTML = _.join([tasks[i].description]);
+
+    contentDiv.append(input, desc);
+    element.append(contentDiv, moveIcon);
+    const list = document.getElementById('list-items');
+
+    list.append(element);
+  }
+}
+
+component();
